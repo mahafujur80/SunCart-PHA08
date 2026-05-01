da14bda564c6d1835ed6eef653ab764ca071ceff
MAHAFUJUR RAHMAN MUNNA <mahafujurrahman4480@gmail.com>
2026-05-01 15:59:17 +0600

    summer care tips section add

--- src/components/Homepage/SummerCareTips/SummerCareTips.jsx
+++ src/components/Homepage/SummerCareTips/SummerCareTips.jsx
@@ -1,14 +1,19 @@
 import SummerCareCard from '@/components/ui/SummerCareCard';
 import React from 'react';
 
-const SummerCareTips = () => {
+const SummerCareTips = async() => {
+    const res  = await fetch('https://suncart-e-com.vercel.app/summerCare.json');
+    const careTipsData = await res.json();
+    
     return (
-         <div className="py-10 space-y-5">
-                    <h1 className="text-2xl font-black text-center" >Summer <span className='text-blue-500'>Care</span> Tips</h1>
-                    <div className="grid grid-cols-3 gap-5"> 
-                        <SummerCareCard/>
-                    </div>
-                </div>
+        <div className="py-10 space-y-5">
+            <h1 className="text-2xl font-black text-center" >Summer <span className='text-blue-500'>Care</span> Tips</h1>
+            <div className="grid grid-cols-2 gap-5">
+               {
+                careTipsData.map(careTips => <SummerCareCard key={careTips.id} careTips={careTips}/>)
+               }
+            </div>
+        </div>
     );
 };
 
--- src/components/ui/SummerCareCard.jsx
+++ src/components/ui/SummerCareCard.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 
-const SummerCareCard = () => {
-  const hydrationData = {
-    title: "Hydration Tips",
-    icon: "💧",
-    tips: [
-      { icon: "🥤", text: "Drink 2–3L Water Daily" },
-      { icon: "🍉", text: "Eat Water-rich Foods" },
-      { icon: "🧃", text: "Avoid Sugary Drinks" },
-      { icon: "🧂", text: "Electrolytes Intake" }
-    ]
-  };
+const SummerCareCard = ({careTips}) => {
+//   const hydrationData = {
+//     title: "Hydration Tips",
+//     icon: "💧",
+//     tips: [
+//       { icon: "🥤", text: "Drink 2–3L Water Daily" },
+//       { icon: "🍉", text: "Eat Water-rich Foods" },
+//       { icon: "🧃", text: "Avoid Sugary Drinks" },
+//       { icon: "🧂", text: "Electrolytes Intake" }
+//     ]
+//   };
+
 
     return (
-        <div className='border p-3 bg-gradient-to-r from-blue-100 to-blue-300 rounded-xl animate__animated animate__fadeInUp animate__slow'>
-            <h1 className='text-center text-xl font-semibold text-gray-800 ' >{hydrationData.title}</h1>
+        <div className='shadow-lg p-3 bg-gradient-to-r from-blue-100 to-blue-300 rounded-xl animate__animated animate__fadeInUp animate__slow'>
+            <h1 className='text-center text-xl font-semibold text-gray-800 ' > {careTips.title}  {careTips.icon}</h1>
             <div className='grid grid-cols-2 gap-x-2'>
-                {hydrationData.tips.map((tip, index) =>{
+                {careTips.tips.map((tip, index) =>{
                     return (
                         <div key={index} className='flex bg-gray-50 rounded hover:scale-105 transition duration-200 p-1  items-center gap-2 mt-3'>
                             <div className='  bg-gray-300 p-3 w-10 h-10 rounded-full flex items-center justify-center'><span className='text-2xl'>{tip.icon}</span></div>
